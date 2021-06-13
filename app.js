134f5bbb61a9bab0f37433559c2cfa6c65ba26dd
rupal121 <rupalsinghid@gmail.com>
2021-06-14 01:24:40 +0530

    Modified Project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
  const https = require("https");
  const bodyParser = require("body-parser");
+ const ejs = require("ejs");
  const app = express();
+ app.use(express.static("public"));
   app.use(bodyParser.urlencoded({extended: true}));
-
+app.set('view engine', 'ejs');
 app.get("/", function(req, res){
 res.sendFile(__dirname + "/index.html"); 	
 	
@@ -17,24 +19,35 @@ const query = req.body.cityName;
 	
 	https.get(url, function(response){
 	console.log(response.statusCode);
-
+    if(response.statusCode === 200){
 	response.on("data", function(data){
 	const weatherData =	JSON.parse(data)
 	const temp = weatherData.main.temp
+
+	
 	const weatherDescription = weatherData.weather[0].description
 	const icon = weatherData.weather[0].icon
 	const imageURL ="http://openweathermap.org/img/wn/"+ icon +"@2x.png"
-	res.write("<p>The weather is currently " + weatherDescription + "<p>");
-	res.write("<h1> The Temperature in " + query +" is " + temp + " degree celcius </h1>")
-	res.write("<img src="+ imageURL + ">");
-	res.send()
 	
-	})	
-	})
+	res.render("list", {temperature: temp, cityname: query, image:imageURL, weather:weatherDescription });
+	
+	
+	});
+
+	}
+	else{
+
+	res.sendFile(__dirname + "/index.html"); 
+}
+	});
 
 
+
+});
+
+app.post("/list", function(req, res){
+	res.redirect("/");
 })
-
 
 
 
